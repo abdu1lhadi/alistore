4b6df0ce3ab4bc2d52d4bc8b126a7a5e32686c90
Abdu1lhadi <abxxdo@gmail.com>
2019-08-03 10:16:51 +0300

    add top 7 list

--- blog/templatetags/post_tag.py
+++ blog/templatetags/post_tag.py
@@ -5,13 +5,13 @@ register = template.Library()
 @register.inclusion_tag('blog/latest_posts.html')
 def latest_posts():
     context = {
-        'l_posts': Post.objects.all()[0:5]
+        'l_posts': Post.objects.all()[0:10]
     }
     return context
 
 @register.inclusion_tag('blog/latest_comments.html')
 def latest_comments():
     context = {
-        'l_Top5': Top5.objects.all()[0:5]
+        'l_Top5': Top5.objects.all()[0:7]
     }
     return context
